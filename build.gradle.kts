3cccaabaceb48746bad5774c61ae4c5b86323d41
Danila Rassokhin <danilarassokhin@gmail.com>
2024-09-22 16:22:41 +0400

    Updated jackson version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 dependencies {
     implementation("io.netty:netty-all:4.1.113.Final")
     implementation("tech.hiddenproject:aide-all:1.3")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.6.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.8.4")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.6.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.6.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.1")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
