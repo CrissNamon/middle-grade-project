edae4bbd0def4cfaa7917e788cdbe56570d0d63a
Danila Rassokhin <danilarassokhin@gmail.com>
2024-11-11 13:39:09 +0400

    Added javadoc

--- src/main/java/ru/danilarassokhin/game/repository/DamageLogRepository.java
+++ src/main/java/ru/danilarassokhin/game/repository/DamageLogRepository.java
@@ -6,12 +6,32 @@ import ru.danilarassokhin.game.entity.DamageLogEntity;
 import ru.danilarassokhin.game.sql.repository.JdbcRepository;
 import ru.danilarassokhin.game.sql.service.TransactionContext;
 
+/**
+ * Repository for {@link DamageLogEntity}.
+ */
 public interface DamageLogRepository extends JdbcRepository<DamageLogEntity, Integer> {
 
+  /**
+   * Counts current damage for given dungeon id.
+   * @param ctx {@link TransactionContext}
+   * @param dungeonId ID of dungeon
+   * @return Sum of damage
+   */
   Long countDamage(TransactionContext ctx, Integer dungeonId);
 
+  /**
+   * Revives dungeon with given id.
+   * @param ctx {@link TransactionContext}
+   * @param dungeonId ID of dungeon
+   */
   void revive(TransactionContext ctx, Integer dungeonId);
 
+  /**
+   * Searches for ids of active players for given dungeon id.
+   * @param ctx {@link TransactionContext}
+   * @param dungeonId ID of dungeon
+   * @return List of player ids
+   */
   List<Integer> findPlayersForActiveDungeon(TransactionContext ctx, Integer dungeonId);
 
 }
--- src/main/java/ru/danilarassokhin/game/repository/DungeonRepository.java
+++ src/main/java/ru/danilarassokhin/game/repository/DungeonRepository.java
@@ -5,8 +5,18 @@ import ru.danilarassokhin.game.entity.data.Dungeon;
 import ru.danilarassokhin.game.sql.repository.JdbcRepository;
 import ru.danilarassokhin.game.sql.service.TransactionContext;
 
+/**
+ * Repository for {@link DungeonEntity}.
+ */
 public interface DungeonRepository extends JdbcRepository<DungeonEntity, Integer> {
 
+  /**
+   * Checks if dungeon exists by given level and code.
+   * @param ctx {@link TransactionContext}
+   * @param level Dungeon level
+   * @param code {@link Dungeon}
+   * @return true if dungeon exists
+   */
   boolean existsByLevelAndCode(TransactionContext ctx, Integer level, Dungeon code);
 
 }
