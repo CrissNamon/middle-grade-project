f8f02fea6c75336fefc9b8c4a76d689e152717b3
Danila Rassokhin <danilarassokhin@gmail.com>
2024-11-11 18:26:06 +0400

    Added lottery

--- src/main/java/ru/danilarassokhin/game/repository/impl/LockRepositoryImpl.java
+++ src/main/java/ru/danilarassokhin/game/repository/impl/LockRepositoryImpl.java
@@ -24,9 +24,10 @@ public class LockRepositoryImpl implements LockRepository {
     var lockTimeOut = String.format(SET_LOCK_TIMEOUT_QUERY, waitTime);
     ctx.query(lockTimeOut).execute();
     ctx.rawQuery(connection -> {
-      var lockStatement = connection.prepareStatement(XACT_LOCK_QUERY);
-      lockStatement.setString(1, name);
-      lockStatement.execute();
+      try(var lockStatement = connection.prepareStatement(XACT_LOCK_QUERY)) {
+        lockStatement.setString(1, name);
+        lockStatement.execute();
+      }
       return null;
     });
   }
