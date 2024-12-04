6cc2574c09d37db674e618e6ad6ca0af994bb173
Danila Rassokhin <danilarassokhin@gmail.com>
2024-12-04 11:10:24 +0400

    Refactoring

--- src/main/java/ru/danilarassokhin/game/resilience/annotation/Cacheable.java
+++ src/main/java/ru/danilarassokhin/game/resilience/annotation/Cacheable.java
@@ -5,10 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks method as cacheable.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Cacheable {
 
+  /**
+   * Cache name.
+   */
   String value();
 
 }
