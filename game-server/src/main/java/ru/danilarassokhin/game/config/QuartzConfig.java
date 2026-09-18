73504a39115982d23ec9b715a96becf5ea086175
Danila Rassokhin <danilarassokhin@gmail.com>
2026-09-18 16:17:38 +0400

    Added Quartz Scheduler

--- game-server/src/main/java/ru/danilarassokhin/game/config/QuartzConfig.java
+++ game-server/src/main/java/ru/danilarassokhin/game/config/QuartzConfig.java
@@ -2,8 +2,6 @@ package ru.danilarassokhin.game.config;
 
 import javax.sql.DataSource;
 
-import java.util.Properties;
-
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
@@ -26,9 +24,6 @@ import tech.hiddenproject.progressive.annotation.GameBean;
 public class QuartzConfig {
 
   public static final String QUARTZ_DATA_SOURCE_NAME = "gameDataSource";
-
-  private static final String QUARTZ_PROPERTY_PREFIX = "quartz";
-  private static final String QUARTZ_PROPERTY_NAME_FORMAT = "org.quartz.%s";
   private static final String QUARTZ_JOB_STORE_DATA_SOURCE_PROPERTY = "org.quartz.jobStore.dataSource";
 
   @GameBean(order = 0)
@@ -41,7 +36,9 @@ public class QuartzConfig {
   @GameBean(order = 1)
   public Scheduler scheduler(PropertiesFactory propertiesFactory, ConnectionProvider quartzConnectionProvider) {
     try {
-      var scheduler = new StdSchedulerFactory(schedulerProperties(propertiesFactory)).getScheduler();
+      var properties = propertiesFactory.getAll();
+      properties.setProperty(QUARTZ_JOB_STORE_DATA_SOURCE_PROPERTY, QUARTZ_DATA_SOURCE_NAME);
+      var scheduler = new StdSchedulerFactory(properties).getScheduler();
       scheduler.setJobFactory(new DiJobFactory(BasicComponentManager.getDiContainer()));
       return scheduler;
     } catch (SchedulerException e) {
@@ -62,15 +59,6 @@ public class QuartzConfig {
   @GameBean(order = 3)
   public QuartzJobContainer quartzJobContainer(Scheduler scheduler, MailSenderJob mailSenderJob) {
     return new QuartzJobContainer(scheduler, mailSenderJob);
-  }
-
-  private Properties schedulerProperties(PropertiesFactory propertiesFactory) {
-    var properties = new Properties();
-    propertiesFactory.getAllForPrefix(QUARTZ_PROPERTY_PREFIX).stringPropertyNames()
-        .forEach(name -> propertiesFactory.getAsString(QUARTZ_PROPERTY_PREFIX + "." + name)
-            .ifPresent(value -> properties.setProperty(QUARTZ_PROPERTY_NAME_FORMAT.formatted(name), value)));
-    properties.setProperty(QUARTZ_JOB_STORE_DATA_SOURCE_PROPERTY, QUARTZ_DATA_SOURCE_NAME);
-    return properties;
   }
 
 }
